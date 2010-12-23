9980b689b52f580383495d68d4b1c5549427f9cf
Francesco Canessa <makevoid@gmail.com>
2010-12-23 02:48:48 +0100

    added view helpers for sinatra

---
+++ lib/voidtools/sinatra/view_helpers.rb
@@ -0,0 +1,13 @@
+module VoidTools
+  module ViewHelpers
+    def link_to(label, path)
+      haml_tag :a, { href: path } do
+        haml_concat label
+      end
+    end
+  
+    def image_tag(url, options={})
+      haml_tag :img, options.merge(src: url)
+    end
+  end
+end
