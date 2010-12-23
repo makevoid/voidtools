f551a5c341878aaecf102248c720ab2250e9c804
Francesco Canessa <makevoid@gmail.com>
2010-12-23 03:15:50 +0100

    newline fix

--- lib/voidtools/sinatra/view_helpers.rb
+++ lib/voidtools/sinatra/view_helpers.rb
@@ -2,9 +2,10 @@ module Voidtools
   module Sinatra
     module ViewHelpers
       def link_to(label, path)
-        haml_tag :a, { href: path } do
-          haml_concat label
-        end
+        # haml_tag :a, { href: path } do
+        #   haml_concat label
+        # end
+        haml_concat "<a href='#{path}'>#{label}</a>"
       end
   
       def image_tag(url, options={})
