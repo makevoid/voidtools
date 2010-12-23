fb7af87a35547ddcf5cebd624d072b2a0ce81f95
Francesco Canessa <makevoid@gmail.com>
2010-12-23 02:51:31 +0100

    check if is a rails app

--- lib/voidtools.rb
+++ lib/voidtools.rb
@@ -2,25 +2,27 @@ module Voidtools
   
   # namespace our plugin and inherit from Rails::Railtie
   # to get our plugin into the initialization process
-  class Railtie < Rails::Railtie
+  if defined?(Rails)
+    class Railtie < Rails::Railtie
  
-    # configure our plugin on boot. other extension points such
-    # as configuration, rake tasks, etc, are also available
-    initializer "voidtools.initialize" do |app|
+      # configure our plugin on boot. other extension points such
+      # as configuration, rake tasks, etc, are also available
+      initializer "voidtools.initialize" do |app|
  
-      # subscribe to all rails notifications: controllers, AR, etc.
-      # ActiveSupport::Notifications.subscribe do |*args|
-      #   event = ActiveSupport::Notifications::Event.new(*args)
-      #   puts "Voidrails - got notification: #{event.inspect}"
-      # end
-      require 'voidtools/dm/form_helpers'
-      ActiveSupport.on_load(:action_view) do
-         module ApplicationHelper
-           include Voidtools::FormHelpers
-         end
+        # subscribe to all rails notifications: controllers, AR, etc.
+        # ActiveSupport::Notifications.subscribe do |*args|
+        #   event = ActiveSupport::Notifications::Event.new(*args)
+        #   puts "Voidrails - got notification: #{event.inspect}"
+        # end
+        require 'voidtools/dm/form_helpers'
+        ActiveSupport.on_load(:action_view) do
+           module ApplicationHelper
+             include Voidtools::FormHelpers
+           end
+        end
+        # require 'voidtools/dm/name_url'
+        # require 'voidtools/dm/paginable'
       end
-      # require 'voidtools/dm/name_url'
-      # require 'voidtools/dm/paginable'
     end
   end
   
