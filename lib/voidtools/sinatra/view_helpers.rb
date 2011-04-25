282d31a77d79ff95e2b08d1f6801530f229d3a0e
Francesco Canessa <makevoid@gmail.com>
2011-04-25 18:35:37 +0200

    added inclued_js and include_css for sinatra

--- lib/voidtools/sinatra/view_helpers.rb
+++ lib/voidtools/sinatra/view_helpers.rb
@@ -14,6 +14,27 @@ module Voidtools
       def image_tag(url, options={})
         haml_tag :img, options.merge(src: url)
       end
+      
+      def include_assets(type, assets)
+        type_js = type == "js"
+        tag = type_js ?  "script" : "link"
+        [assets].flatten.each do |asset|
+          options = if type_js
+            { type: "text/javascript", src: asset  }
+          else
+            { rel: "stylesheet", href: asset }
+          end
+          haml_tag tag, options
+        end
+      end
+      
+      def include_js(assets)
+        include_assets "js", assets
+      end
+      
+      def include_css(assets)
+        include_assets "css", assets
+      end
     end
   end
 end
