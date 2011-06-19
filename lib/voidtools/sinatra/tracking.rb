74b01064836b585cdb9dab32709901c86209be9c
Francesco Canessa <makevoid@gmail.com>
2011-06-19 07:57:48 +0200

    check if env is not dev

--- lib/voidtools/sinatra/tracking.rb
+++ lib/voidtools/sinatra/tracking.rb
@@ -1,48 +1,74 @@
+# Voidtools::Tracking
+
+# Usage:
+# 
+#   :javascript
+#     #{crazyegg}
+#     #{gauges("id")}
+
+
 module Voidtools 
 module Tracking
+  
+  def track_run(&block)
+    if defined?(Rails)
+      yield if Rails.env != "development"
+    else
+      yield if ENV["RACK_ENV"] != "development"
+    end
+  end
+  
   def analytics(id, domain) # before the end of head tag
-   <<-FUN
-   var _gaq = _gaq || [];
-   _gaq.push(['_setAccount', '#{id}']);
-   //_gaq.push(['_setDomainName', '.#{domain}']);
-   _gaq.push(['_trackPageview']);
+    track_run do
+      <<-FUN
+      var _gaq = _gaq || [];
+      _gaq.push(['_setAccount', '#{id}']);
+      //_gaq.push(['_setDomainName', '.#{domain}']);
+      _gaq.push(['_trackPageview']);
 
-   (function() {
-     var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-     ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-     var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-   })();
+      (function() {
+        var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+        ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+        var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+      })();
 FUN
- 
+    end
   end
   
   def mixpanel
-    # ...
+    track_run do
+      # ...
+    end
   end
   
   def crazyegg # before the end of body tag
-    <<-FUN 
-    setTimeout(function(){var a=document.createElement("script");
-    var b=document.getElementsByTagName('script')[0];
-    a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0011/5433.js";
-    a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
+  
+    track_run do   
+      <<-FUN 
+      setTimeout(function(){var a=document.createElement("script");
+      var b=document.getElementsByTagName('script')[0];
+      a.src=document.location.protocol+"//dnn506yrbagrg.cloudfront.net/pages/scripts/0011/5433.js";
+      a.async=true;a.type="text/javascript";b.parentNode.insertBefore(a,b)}, 1);
 FUN
+    end
 
   end
   
   def gauges(id) # before the end of body tag
-    <<-FUN 
-    (function() {
-        var t   = document.createElement('script');
-        t.type  = 'text/javascript';
-        t.async = true;
-        t.id    = 'gauges-tracker';
-        t.setAttribute('data-site-id', '#{id}');
-        t.src = '//secure.gaug.es/track.js';
-        var s = document.getElementsByTagName('script')[0];
-        s.parentNode.insertBefore(t, s);
-      })();
+    track_run do
+      <<-FUN 
+      (function() {
+          var t   = document.createElement('script');
+          t.type  = 'text/javascript';
+          t.async = true;
+          t.id    = 'gauges-tracker';
+          t.setAttribute('data-site-id', '#{id}');
+          t.src = '//secure.gaug.es/track.js';
+          var s = document.getElementsByTagName('script')[0];
+          s.parentNode.insertBefore(t, s);
+        })();
 FUN
+    end
   end
 end
 end
