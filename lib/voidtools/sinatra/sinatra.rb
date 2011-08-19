8c66592a782877ed15621857f652c9e84dcb3815
makevoid <makevoid@gmail.com>
2011-08-19 06:35:56 +0200

    tracking autoloads

--- lib/voidtools/sinatra/sinatra.rb
+++ lib/voidtools/sinatra/sinatra.rb
@@ -1,2 +1,11 @@
 path = File.expand_path "../", __FILE__
 require "#{path}/view_helpers"
+
+require "#{path}/tracking"
+module Voidtools
+  module Sinatra
+    module ViewHelpers
+      include Tracking
+    end
+  end
+end
